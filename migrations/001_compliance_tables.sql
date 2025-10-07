34925ada99120149ce6583658bd304b224e3f754
elevateforhumanity <elevateforhumanity@gmail.com>
2025-10-07 02:43:33 +0000

    feat: add WIOA compliance middleware, migrations, and utilities
    - Error handler middleware
    - Validation middleware
    - Database migrations for compliance tables
    - Encryption utilities
    - Protected route component
    - Auth context and hooks
    
    Co-authored-by: Ona <no-reply@ona.com>

---
+++ migrations/001_compliance_tables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:250b0f52ae5c449ead4efecf2cfa6911b23f9308beb6c3a92a2d0da4cddfb9e2
+size 1594
---
+++ migrations/002_wioa_compliance_tables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3cbd361b2dd421bfcf159887cfddcd06f84e2acde30180ed0b886c6b86731eef
+size 17522
